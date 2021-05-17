1b60605abce83b9d1fd4d6b79d5d9035fce4355e
Stefano Saffran <stefanoas@gmail.com>
2021-05-16 22:10:29 -0400

    :sparkles: feat: configure app.tsx and add global styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+// import App from 'next/app'
+import type { AppProps /*, AppContext */ } from 'next/app';
+import { GlobalStyles } from 'styles/global';
+import Head from 'next/head';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </>
+  );
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default App;
