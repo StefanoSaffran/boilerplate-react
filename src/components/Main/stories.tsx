c2734584e62e782c691d82b1c0e4640eec64d5e7
Stefano Saffran <stefanoas@gmail.com>
2021-05-18 15:13:58 -0400

    :wrench: feat: configure storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,20 @@
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react';
+
+import { Main } from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs],
+} as Meta;
+
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text(
+      'Description',
+      'TypeScript, ReactJS, NextJS e Styled Components',
+    )}
+  />
+);
