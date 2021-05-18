f48ecd9802349cfc2363953f1190fc2913ec25f2
Stefano Saffran <stefanoas@gmail.com>
2021-05-18 09:23:14 -0400

    :sparkles: feat: create first styles with styled components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
-export const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
+import * as S from './styles';
 
-    <p>Testando o merge</p>
-  </main>
+export const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.Wrapper>
 );
