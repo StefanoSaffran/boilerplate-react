47935c301342f6cec4c7eacb5520050cd1bc8fae
Stefano Saffran <stefanoas@gmail.com>
2021-05-19 09:47:10 -0400

    :wrench: chore: finish setting up storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,4 +1,3 @@
-import { withKnobs, text } from '@storybook/addon-knobs';
 import { Story, Meta } from '@storybook/react';
 
 import { Main } from '.';
@@ -6,15 +5,21 @@ import { Main } from '.';
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
+  parameters: {
+    controls: {
+      hideNoControlsWarning: true,
+    },
+  },
+  args: {
+    title: 'asdasdaddasda',
+    description: 'asdasdasdasd',
+  },
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components',
-    )}
-  />
-);
+export const Basic: Story = args => <Main {...args} />;
+
+export const Default: Story = args => <Main {...args} />;
+Default.args = {
+  title: '123',
+  description: '123',
+};
